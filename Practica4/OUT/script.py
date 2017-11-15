74f55522af69f7c9100e3d41afa0b280951c8419
hero_katas@homail.com <hero_katas@hotmail.com>
2017-11-15 14:29:21 -0600

    script para abrir terminales

---
+++ Practica4/OUT/script.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+os.system("x-terminal-emulator -e ./practica4 0")
+os.system("x-terminal-emulator -e ./practica4 1")
+#os.system("x-terminal-emulator -e 'cat script.py'")
+#os.system("deepin-terminal 'echo carlos | sudo -S apt-get update'")
